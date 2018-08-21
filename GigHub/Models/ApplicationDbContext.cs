38337b12eb7b701d01a637aa3c53a44c6c216ef8
David Mac <davidmmaceda@gmail.com>
2018-08-21 12:41:49 +0800

    Added Following Feature and Reformatted HomeController

--- GigHub/Models/ApplicationDbContext.cs
+++ GigHub/Models/ApplicationDbContext.cs
@@ -8,6 +8,7 @@ namespace GigHub.Models
         public DbSet<Gig> Gigs { get; set; }
         public DbSet<Genre> Genres { get; set; }
         public DbSet<Attendance> Attendances { get; set; }
+        public DbSet<Following> Followings { get; set; }
 
 
         public ApplicationDbContext()
@@ -23,8 +24,20 @@ namespace GigHub.Models
         protected override void OnModelCreating(DbModelBuilder modelBuilder) // implements fluent API
         {
             modelBuilder.Entity<Attendance>()
-                .HasRequired(a => a.Gig).WithMany()
+                .HasRequired(a => a.Gig)
+                .WithMany()
                 .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<ApplicationUser>()
+                .HasMany(u => u.Followers)  // An application user has many Followers
+                .WithRequired(f => f.Followee) // has a required Followee
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<ApplicationUser>()
+                .HasMany(u => u.Followees) // An application user has many Followees
+                .WithRequired(f => f.Follower)// has a required Follower 
+                .WillCascadeOnDelete(false);
+
 
             base.OnModelCreating(modelBuilder);
         }
