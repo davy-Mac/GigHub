ea6bef5a4f9008b564d253c4a314b2a83288c188
David Maceda <David.Maceda@duratecaustralia.com.au>
2018-09-07 09:53:28 +0800

    Implemented Moment to format dateTime and LESS

--- GigHub/App_Start/BundleConfig.cs
+++ GigHub/App_Start/BundleConfig.cs
@@ -7,9 +7,12 @@ namespace GigHub
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            bundles.Add(new ScriptBundle("~/bundles/lib").Include(
                         "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/underscore-min.js"));
+                        "~/Scripts/underscore-min.js",
+                        "~/Scripts/moment.js",
+                        "~/Scripts/bootstrap.js",
+                        "~/Scripts/bootbox.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -19,9 +22,11 @@ namespace GigHub
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/bootbox.min.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                        //"~/Scripts/bootstrap.js", // this packages have been moved to the lib bundle
+                        //"~/Scripts/respond.js",   // this packages have been moved to the lib bundle
+                        //"~/Scripts/bootbox.min.js"   // this packages have been moved to the lib bundle
+                        //          ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
