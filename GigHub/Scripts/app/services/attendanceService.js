40d0461c5d931b09127d80e7ed770280df4f5741
David Maceda <David.Maceda@duratecaustralia.com.au>
2018-09-18 13:40:03 +0800

    Separation of javaScript and modularization

---
+++ GigHub/Scripts/app/services/attendanceService.js
@@ -0,0 +1,21 @@
+﻿var AttendanceService = function () {
+    var createAttendance = function (gigId, done, fail) {
+        $.post("/api/attendances", { gigId: gigId })
+            .done(done)
+            .fail(fail);
+    }
+
+    var deleteAttendance = function (gigId, done, fail) {
+        $.ajax({
+                url: "/api/attendances/" + gigId, /*the button holds the data attribute with the gig id*/
+                method: "DELETE"
+            })
+            .done(done)
+            .fail(fail);
+    };
+
+    return {
+        createAttendance: createAttendance,
+        deleteAttendance: deleteAttendance
+    }
+}();
