0703f82e9cfdf21f78dffee429d4decbf8fade09
David Maceda <David.Maceda@duratecaustralia.com.au>
2018-10-01 13:41:55 +0800

    Refactored Project Structure To A More Organized Solution

---
+++ GigHub/Core/Dtos/NotificationDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using GigHub.Core.Models;
+
+namespace GigHub.Core.Dtos
+{
+    public class NotificationDto
+    {
+        public DateTime DateTime { get; set; } // "private" so it's always valid cannot be changed once set
+
+        public NotificationType Type { get; set; } // "private" so it's always valid cannot be changed once set
+
+        public DateTime? OriginalDateTime { get; set; } // "private" so it's always valid cannot be changed once set
+
+        public string OriginalVenue { get; set; } // "private" so it's always valid cannot be changed once set
+
+        public GigDto Gig { get; set; } // "private" so it's always valid cannot be changed once set
+    }
+}
